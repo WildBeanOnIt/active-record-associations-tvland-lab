2c4bd0a191f2ad361621e7aab51520c31a4af4ab
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-02-27 20:11:56 -0600

    All done

--- db/migrate/009_add_date_to_season_to_show.rb
+++ db/migrate/009_add_date_to_season_to_show.rb
@@ -1,5 +1,6 @@
 class AddDateToSeasonToShow < ActiveRecord::Migration[5.1]
     def change
-        add_column :shows, :date, :string
+        add_column :shows, :day, :string
+        add_column :shows, :season, :string
     end
 end
