effff455cd9cedec17f6dc6f5fb7c3c7e96517ae
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-02-27 19:20:49 -0600

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,8 @@
 class Character < ActiveRecord::Base
-    belongs_to :actor
     belongs_to :show
+    belongs_to :actor
+
+    def say_catchphrase
+        "#{self.name} always says: #{self.catchphrases}"
+    end
 end
